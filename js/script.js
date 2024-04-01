86170b773a040e80451ec508b953f8501dc285c6
Sander Nilsen <sander.nil@hotmail.com>
2024-04-01 20:30:38 +0200

    index.html update
    hamburger-menu, html, css & js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Hamburger-menu
+
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navLinks = document.querySelector(".nav-links");
+
+hamburgerMenu.addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("active");
+    navLinks.classList.toggle("active");
+});
